8103991b9c49cf3619697713789c7189245a4970
Amanda EB <66978499+amandaeb@users.noreply.github.com>
2022-12-01 05:21:50 -0600

    Update script.js

--- script.js
+++ script.js
@@ -144,7 +144,7 @@ let benchShadowTwoAnim = gsap.fromTo(benchShadowTwo,
 );
 let roofAnim =  gsap.fromTo(roof, 
     {x:-1000},
-    {x:800,
+    {x:200,
         delay:1.2,
         ease:"slow (0.7,0.7, false)"
     }
